763cfbb097f64911d4441763940260663bfe7ee9
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-10-01 00:09:20 +0530

    Update records

---
+++ update-records/update-set-where-2.sql
@@ -0,0 +1,5 @@
+UPDATE person
+SET first_name = 'Harry',
+    last_name = 'Manchanda',
+    email = 'harry.manchanda@hotmail.com'
+WHERE id = 2005
---
+++ update-records/update-set-where.sql
@@ -0,0 +1,3 @@
+UPDATE person
+SET email = 'harry@hotmail.com'
+WHERE id = 2005
